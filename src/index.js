7470a88bec22c652e070e195757c473a25efdabc
Deep <deepchakraborty0810@gmail.com>
2023-07-27 01:13:49 +0530

    update

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 
-import App from "./src/App";
-import "./src/index.css";
+import App from "./App";
+import "./index.css";
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
